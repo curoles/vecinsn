893bb0823dfa907c5eefbaacabb710c0ab77da61
Igor Lesik <ilesik@tachyum.com>
2021-07-08 00:58:16 -0700

    experimenting with array

--- test/generic/test_array.cpp
+++ test/generic/test_array.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <cstdint>
 #include <cassert>
+#include <cmath>
 #include <type_traits>
 
 #include "vx/vxarray.hpp"
@@ -9,12 +10,12 @@ static bool test_array()
 {
     using namespace vx;
 
-    vx::array<int16_t, 8> a {pv: {{1,2,3,4,5,6,7,8}}};
-    vx::array<int16_t, 8> b {pv: {{4,3,2,1,8,7,5,6}}};
+    vx::array<int16_t, 8> a {1,2,3,4,5,6,7,8};
+    vx::array<int16_t, 8> b {4,3,2,1,8,7,5,6};
 
     static_assert(a.PSz == 8 and a.Cnt == 1);
 
-    assert(a.pv[0][5] == 6);
+    assert(a.data()[0][5] == 6);
     assert(a[6] == 7);
 
     a.add(b);
@@ -23,13 +24,44 @@ static bool test_array()
     a.sub(b);
     assert(a[6] == 7);
 
+    auto&& c = a + b;
+    assert(c.at(7) == (8+6));
+
+    return true;
+}
+
+#if 0
+template<class T>
+typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type
+almost_equal(T x, T y, unsigned int ulp)
+{
+    // the machine epsilon has to be scaled to the magnitude of the values used
+    // and multiplied by the desired precision in ULPs (units in the last place)
+    return std::fabs(x-y) <= std::numeric_limits<T>::epsilon() * std::fabs(x+y) * ulp
+        // unless the result is subnormal
+        || std::fabs(x-y) < std::numeric_limits<T>::min();
+}
+#endif
+
+static bool test_array_forloop()
+{
+    using namespace vx;
+
+    vx::array<float, 8> d;
+    d.fill(777.123);
+
+    for (const auto v : d) {
+        //printf("%f\n", v);
+        assert(std::fabs(v - 777.123) < 0.001);
+    }
+
     return true;
 }
 
 using TestFun = bool (*)();
 
 static TestFun tests[] = {
-    test_array
+    test_array, test_array_forloop
 };
 
 int main(int, char**)
--- vx/vxarray.hpp
+++ vx/vxarray.hpp
@@ -8,6 +8,9 @@
  */
 #pragma once
 
+#include <stdexcept>
+#include <iterator>
+
 #include "vx/vxtypes.hpp"
 #include "vx/vxops.hpp"
 #include "vx/vxfun.hpp"
@@ -19,8 +22,9 @@ namespace vx {
 /// Array of Vectors that pretends to be a vector of base-type elements.
 ///
 template <typename T, std::size_t Sz>
-struct array
+class array
 {
+public:
     static constexpr std::size_t PSz =
         recommended_vector_size_for_array<T, Sz>() / sizeof(T);
 
@@ -28,18 +32,49 @@ struct array
 
     using value_type = T;
     using reference = value_type&;
+    using const_reference = const reference;
+    using size_type = std::size_t;
 
     using pv_type = typename vx::make<T, PSz>::type; ///< type of the packed vector
+    using pv_array = pv_type[Cnt];
 
+private:
     pv_type pv[Cnt];
 
+
+public:
+    array() = default;
     array(const array&) = default;
+
+    array(std::initializer_list<T> list) {
+        for (std::size_t pos = 0; const T v : list) {
+            at(pos++) = v;
+        }
+    }
 
     array& operator=(const array&) = default;
 
+    pv_array& data() {return pv;}
+
     /// Returns reference to n-th element.
-    T& operator[](std::size_t i) const {
-        return pv[i/PSz][i%PSz];
+    reference operator[](std::size_t pos) {
+        return pv[pos/PSz][pos%PSz];
+    }
+
+    const_reference operator[](std::size_t pos) const {
+        return pv[pos/PSz][pos%PSz];
+    }
+
+    /// Returns a reference to the element at specified location pos,
+    /// with bounds checking. 
+    reference at(size_type pos) {
+        if (pos >= Sz) throw std::out_of_range("vx::array");
+        return operator[](pos);
+    }
+
+    const_reference at(size_type pos) const  {
+        if (pos >= Sz) throw std::out_of_range("vx::array");
+        return operator[](pos);
     }
 
     /// Adds `this[n] += other[n]`.
@@ -65,7 +100,59 @@ struct array
         return *this;
     }
 
+    /// Assigns the given value value to all elements in the container.
+    void fill(const T& value) {
+        for (std::size_t chunk = 0; chunk < Cnt; ++chunk) {
+            vx::fill(pv[chunk], value);
+        }
+    }
+
+    struct const_iterator
+    {
+        using iterator_category = std::forward_iterator_tag;
+        //using difference_type   = std::ptrdiff_t;
+        using value_type        = T;
+        using pointer           = value_type*;
+        using reference         = value_type&;
+
+        const_iterator(vx::array<T,Sz>& ar, std::size_t pos):
+            ar_(ar), pos_(pos) {}
+
+        const reference operator*() const { return ar_.at(pos_); }
+        //pointer operator->() { return ptr_; }
+        const_iterator& /*prefix*/ operator++() { pos_++; return *this; }
+        const_iterator operator++(int) {
+            const_iterator tmp = *this; ++(*this); return tmp; }
+
+        friend bool operator== (const const_iterator& a, const const_iterator& b) {
+            return a.pos_ == b.pos_;
+        };
+        friend bool operator!= (const const_iterator& a, const const_iterator& b) {
+            return a.pos_ != b.pos_;
+        };
+
+    private:
+        vx::array<T,Sz>& ar_;
+        std::size_t pos_;
+    };
+
+    const_iterator cbegin() { return const_iterator(*this, 0); }
+    const_iterator cend()   { return const_iterator(*this, Sz); }
+
+    const_iterator begin() { return const_iterator(*this, 0); }
+    const_iterator end()   { return const_iterator(*this, Sz); }
 };
+
+template <typename T, std::size_t Sz>
+vx::array<T, Sz> operator+(
+    const vx::array<T, Sz>& first,
+    const vx::array<T, Sz>& second
+)
+{
+    vx::array<T,Sz> a(first);
+    a.add(second);
+    return a;
+}
 
 
 } // namespace vx
